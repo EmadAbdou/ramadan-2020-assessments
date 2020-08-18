0d81db93963d92d872c131822f911df1615e0089
EmadAbdou <emad_abdou7@yahoo.com>
2020-08-18 21:09:02 +0200

    Finish All Required Tasks

--- server/data/video-requests.data.js
+++ server/data/video-requests.data.js
@@ -1,52 +1,73 @@
-var VideoRequest = require('./../models/video-requests.model');
+const VideoRequest = require('./../models/video-requests.model');
+const User = require('./../models/user.model');
 
 module.exports = {
-  createRequest: (vidRequestData) => {
-    let newRequest = new VideoRequest(vidRequestData);
-    return newRequest.save();
-  },
+    createRequest: async(vidRequestData) => {
+        const userId = vidRequestData.author_id;
+        if (userId) {
+            const userObj = await User.findOne({ _id: userId });
+            vidRequestData.author_name = userObj.author_name;
+            vidRequestData.author_email = userObj.author_email;
+        }
+        let newRequest = new VideoRequest(vidRequestData);
+        return newRequest.save();
+    },
 
-  getAllVideoRequests: (top) => {
-    return VideoRequest.find({}).sort({ submit_date: '-1' }).limit(top);
-  },
+    getAllVideoRequests: (filterBy) => {
+        const filter = filterBy === 'all' ? {} : { status: filterBy };
+        return VideoRequest.find(filter).sort({ submit_date: '-1' });
+    },
 
-  searchRequests: (topic) => {
-    return VideoRequest.find({ topic_title: topic })
-      .sort({ addedAt: '-1' })
-      .limit(top);
-  },
+    searchRequests: (topic, filterBy) => {
+        const filter = filterBy === 'all' ? {} : { status: filterBy };
+        return VideoRequest.find({ topic_title: { $regex: topic, $options: 'i' }, ...filter })
+            .sort({ addedAt: '-1' });
+        // .limit(top);
+    },
 
-  getRequestById: (id) => {
-    return VideoRequest.findById({ _id: id });
-  },
+    getRequestById: (id) => {
+        return VideoRequest.findById({ _id: id });
+    },
 
-  updateRequest: (id, status, resVideo) => {
-    const updates = {
-      status: status,
-      video_ref: {
-        link: resVideo,
-        date: resVideo && new Date(),
-      },
-    };
+    updateRequest: (id, status, resVideo) => {
+        const updates = {
+            status: status,
+            video_ref: {
+                link: resVideo,
+                date: resVideo && new Date(),
+            },
+        };
 
-    return VideoRequest.findByIdAndUpdate(id, updates, { new: true });
-  },
+        return VideoRequest.findByIdAndUpdate(id, updates, { new: true });
+    },
 
-  updateVoteForRequest: async (id, vote_type) => {
-    const oldRequest = await VideoRequest.findById({ _id: id });
-    const other_type = vote_type === 'ups' ? 'downs' : 'ups';
-    return VideoRequest.findByIdAndUpdate(
-      { _id: id },
-      {
-        votes: {
-          [vote_type]: ++oldRequest.votes[vote_type],
-          [other_type]: oldRequest.votes[other_type],
-        },
-      }
-    );
-  },
+    updateVoteForRequest: async(id, vote_type, user_id) => {
+        const oldRequest = await VideoRequest.findById({ _id: id });
+        const other_type = vote_type === 'ups' ? 'downs' : 'ups';
 
-  deleteRequest: (id) => {
-    return VideoRequest.deleteOne({ _id: id });
-  },
+        const oldVoteList = oldRequest.votes[vote_type];
+        const oldOtherList = oldRequest.votes[other_type];
+
+        if (!oldVoteList.find(u => u == user_id)) {
+            oldVoteList.push(user_id);
+        } else {
+            oldVoteList.splice(user_id);
+        }
+
+        if (oldOtherList.includes(user_id)) {
+            oldOtherList.splice(user_id);
+        }
+
+
+        return VideoRequest.findByIdAndUpdate({ _id: id }, {
+            votes: {
+                [vote_type]: oldVoteList,
+                [other_type]: oldOtherList,
+            },
+        }, { new: true });
+    },
+
+    deleteRequest: (id) => {
+        return VideoRequest.deleteOne({ _id: id });
+    },
 };
